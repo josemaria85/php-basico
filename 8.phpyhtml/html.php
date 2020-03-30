9d30a67d30f99f6fef897eb714c35d2e43e93aa4
josemaria85 <josele3985@gmail.com>
2020-03-30 15:41:47 +0200

    html y php
    html y php

---
+++ 8.phpyhtml/html.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Hola soy un html</h2>
+    <?php
+        $variable="25";
+        $variable2="5";
+        echo $variable*$variable2;
+    ?>
+</body>
+</html>
