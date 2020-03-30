20a70b764865e3d638091355593c8450c91d3249
josemaria85 <josele3985@gmail.com>
2020-03-30 13:18:21 +0200

    Fin variables
    Finalizacion variables

---
+++ 2.variables/index.php
@@ -0,0 +1,12 @@
+<?php
+
+//definicion y asignacion de variables
+//pueden ser globales o locales en las funciones 
+$variable="Texto";
+$numero=11;
+
+//mostrar en el html
+echo $variable . $numero ."\n";// el punto sirve para concatenar \n saltar de linea.
+//echo $numero;
+
+?>
